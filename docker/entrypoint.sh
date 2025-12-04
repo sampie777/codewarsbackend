50ef4d4b3b1dbda6d06dc65c991388a720b90908
S. Jansen <samuelanton@hotmail.com>
2025-12-04 22:37:08 +0200

    fix(docker): Don't crash when loading invalid secret names

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,10 +4,17 @@
 if [ -d "/run/secrets" ]; then
   for path in /run/secrets/*; do
     file="$(basename "${path}")"
+
+    if [[ "$file" = "kubernetes.io" ]]; then
+      echo "Skipping file '${file}'"
+      continue
+    fi
+
     echo "Exporting secret '${file}' as environment variable"
     export $file=$(cat "${path}") || echo "Failed to export secret '${file}'"
   done
 fi
 
 # Run application
+echo "Running application..."
 java -jar /app/codewarsbackend.jar
